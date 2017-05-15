99f036e02ed88c97aca91156b0344c90dbc4e97c
RadhiaGaddouri <rgaddouri@cesi.fr>
2017-05-15 11:51:47 +0200

    Delete package-info.java

--- insane_vehicles_code java/src/fr/exia/insanevehicles/element/motionless/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package contains classes to produce MotionLessElement.
- * There is a factory : MotionlessElementsFactory.
- *
- * @author Jade
- * @version 0.1
- */
-
-package fr.exia.insanevehicles.element.motionless;
