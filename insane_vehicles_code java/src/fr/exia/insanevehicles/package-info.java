2a370a132bcaf654513c2e11013ed5c1c95ad7ee
RadhiaGaddouri <rgaddouri@cesi.fr>
2017-05-15 11:49:12 +0200

    Delete package-info.java

--- insane_vehicles_code java/src/fr/exia/insanevehicles/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package contains classes to start the game.
- * The main() function is in InsaneVehicles class.
- *
- * @author Jade
- * @version 0.1
- */
-
-package fr.exia.insanevehicles;
